7cc5107b6530277b40eb628f1304f7f4823724c2
Jomo Fisher <jomof@google.com>
2024-08-02 15:18:14 +0000

    Printenv

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
-mkdir -p .secrets && echo $GCP_CLOZE_DATA_CACHE_KEY > .secrets/bazel-cache-key.json
+echo Making .secrets directory
+mkdir -p .secrets
+echo Making .secrets/bazel-cache-key.json
+echo $GCP_CLOZE_DATA_CACHE_KEY > .secrets/bazel-cache-key.json
+echo Printing environment
+printenv
