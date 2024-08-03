aac4cd77c0d4eb0612985bd00a5c2818aebdd22c
Jomo Fisher <jomof@google.com>
2024-08-03 20:38:22 +0000

    Tweeks

--- mecab/tagger.py
+++ mecab/tagger.py
@@ -1,7 +1,6 @@
 import MeCab
-import os
 import unidic
 
 def get_mecab_tagger():
-    unidic.DICDIR = "unidic/dicdir"
+    unidic.DICDIR = "/usr/local/python/3.10.13/lib/python3.10/site-packages/unidic/dicdir"
     return MeCab.Tagger('-d "{}"'.format(unidic.DICDIR))
--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo Making .secrets/bazel-cache-key.json
 echo $GCP_CLOZE_DATA_CACHE_KEY > .secrets/bazel-cache-key.json
 
 # Set up unidic
-wget https://storage.googleapis.com/cloze-data-bazel-cache/unidic-3.1.0.zip
-unzip unidic-3.1.0.zip
-touch unidic/mecabrc
+#wget https://storage.googleapis.com/cloze-data-bazel-cache/unidic-3.1.0.zip
+#unzip unidic-3.1.0.zip
+#touch unidic/mecabrc
