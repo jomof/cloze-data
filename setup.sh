745b3600d3b559df3415f38d0fb3fa5e75eb8dc9
Jomo Fisher <jomof@google.com>
2024-08-03 20:21:37 +0000

    Try

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@ echo Making .secrets directory
 mkdir -p .secrets
 echo Making .secrets/bazel-cache-key.json
 echo $GCP_CLOZE_DATA_CACHE_KEY > .secrets/bazel-cache-key.json
-echo Printing environment
-printenv | sort
+
+# Set up unidic
+wget https://storage.googleapis.com/cloze-data-bazel-cache/unidic-3.1.0.zip
+unzip unidic-3.1.0.zip
+touch unidic/mecabrc
