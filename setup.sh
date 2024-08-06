d23c4f01f69f048669c366249718cc491c00bb73
Jomo Fisher <jomof@google.com>
2024-08-06 10:43:07 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ workspace_dir=$(find_workspace_dir)
 
 echo Making $workspace_dir/.secrets directory
 mkdir -p $workspace_dir/.secrets
-# echo $GCP_CLOZE_DATA_CACHE_KEY > $workspace_dir/.secrets/bazel-cache-key.json
+echo $GCP_CLOZE_DATA_CACHE_KEY > $workspace_dir/.secrets/bazel-cache-key.json
 
 sudo npm install -g npm@10.8.2
 sudo npm install -g @bazel/bazelisk
