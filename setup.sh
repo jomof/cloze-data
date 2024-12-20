b1497834f0d6d5f643ac360811dea3809033ac24
jomofisher <jomofisher@gmail.com>
2024-12-20 16:52:03 +0000

    Move

--- setup.sh
+++ setup.sh
@@ -18,11 +18,11 @@ echo Making $workspace_dir/.secrets directory
 mkdir -p $workspace_dir/.secrets
 echo $GCP_CLOZE_DATA_CACHE_KEY > $workspace_dir/.secrets/bazel-cache-key.json
 
-sudo npm install -g npm@10.8.2
-sudo npm install -g @bazel/bazelisk
-sudo pip install mecab-python3==1.0.9
-sudo pip install unidic==1.1.0
-sudo python -m unidic download
+# sudo npm install -g npm@10.8.2
+# sudo npm install -g @bazel/bazelisk
+# sudo pip install mecab-python3==1.0.9
+# sudo pip install unidic==1.1.0
+# sudo python -m unidic download
 export HISTFILE=$workspace_dir/.bash_history
 
 
