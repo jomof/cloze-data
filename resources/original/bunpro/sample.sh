96d2205bef40dc7d1133d6b0b8cdd29ed9e7c86d
jomofisher <jomofisher@gmail.com>
2024-12-23 13:00:25 +0000

    Last of the samples

--- resources/original/bunpro/sample.sh
+++ resources/original/bunpro/sample.sh
@@ -1,3 +1,3 @@
 cd /workspaces/cloze-data/resources/original/bunpro
 mkdir -p sample
-ls all | shuf -n 60 | xargs -I{} cp "all/{}" "sample/"
+ls all | shuf -n 1000 | xargs -I{} cp "all/{}" "sample/"
