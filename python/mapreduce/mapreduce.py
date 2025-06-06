832e4c9a70264c1ecb14350b2d56d8acf66feb40
Jomo Fisher <jomof@google.com>
2025-06-06 22:53:35 +0000

    Switch to yaml

--- python/mapreduce/mapreduce.py
+++ python/mapreduce/mapreduce.py
@@ -11,8 +11,7 @@ from threading import Lock
 import aiofiles  
 import signal
 import yaml
-import json
-
+from collections import OrderedDict
 
 class ConsoleDisplay:
     """
@@ -406,6 +405,31 @@ class ConsoleDisplay:
             'time': time.monotonic()
         })
 
+class MyDumper(yaml.Dumper):
+    def increase_indent(self, flow=False, indentless=False):
+        return super().increase_indent(flow, False)
+
+def str_presenter(dumper, data):
+    # If the string has newline characters, use block style
+    if '\n' in data:
+        return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.items())
+
+def dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+
+yaml.add_representer(str, str_presenter, Dumper=MyDumper)
+yaml.add_representer(OrderedDict, dict_representer, Dumper=MyDumper)
+yaml.add_constructor('tag:yaml.org,2002:map', dict_constructor, Loader=yaml.Loader)
+
+def dump_yaml(data):
+    return yaml.dump(data, Dumper=MyDumper, allow_unicode=True, default_flow_style=False, width=100, sort_keys=False)
+
+def dump_yaml_file(data, file):
+    return yaml.dump(data, file, Dumper=MyDumper, allow_unicode=True, default_flow_style=False, width=100, sort_keys=False)
         
 
 class MapReduce:
@@ -430,7 +454,7 @@ class MapReduce:
         fold_func_name: str = 'folding',
         fold_func=None,
 
-        serialize_func=lambda raw: json.dumps(raw, ensure_ascii=False, indent=4),
+        serialize_func=lambda raw: dump_yaml(raw),
 
         output_dir: str = None,
         initial_accumulator=None,
